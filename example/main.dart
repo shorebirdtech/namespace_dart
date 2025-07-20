58da201cb8e1d297fd25cd8894a4adf6409c72f1
Eric Seidel <eric@shorebird.dev>
2025-07-20 16:19:32 -0700

    fix: teach example to get nsc token, still doesn't work yet

--- example/main.dart
+++ example/main.dart
@@ -12,14 +12,26 @@ final transport = protocol.Transport(
   httpClient: createHttpClient(),
 );
 
-void main() async {
-  final token = Platform.environment['NSC_TOKEN'];
-  if (token == null) {
-    throw Exception('NSC_TOKEN is not set');
+String getNSCToken() {
+  // First try to run check-login to see if nsc is installed and logged in.
+  final checkResult = Process.runSync('nsc', ['auth', 'check-login']);
+  if (checkResult.exitCode != 0) {
+    throw Exception(
+      'nsc is not installed or not logged in.\n'
+      'see https://namespace.so/docs/reference/cli/installation',
+    );
   }
+  // Get a dev token:
+  final tokenResult = Process.runSync('nsc', ['auth', 'generate-dev-token']);
+  if (tokenResult.exitCode != 0) {
+    throw Exception('Failed to generate dev token');
+  }
+  return tokenResult.stdout;
+}
 
+void main() async {
+  final token = getNSCToken();
   ListInstancesRequest request = ListInstancesRequest();
-
   final response = await ComputeServiceClient(transport).listInstances(
     request,
     headers: Headers()..['authorization'] = 'Bearer $token',
